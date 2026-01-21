20338e1ad1f4b163912bdb596b47e83f7dbcf2c3
iain @garnet <iainvisa@gmail.com>
2026-01-21 21:18:41 +0100

    100 epochs

--- projections/main.py
+++ projections/main.py
@@ -580,7 +580,7 @@ def image_classifier(mode="double_connected", train = True, decode=False, view=F
         i_criterion = DiceLoss
         #i_optimizer = optim.SGD(net.r_net.parameters(), lr=0.001)
         i_optimizer = optim.AdamW(net.r_net.parameters(), lr=0.001)
-        epochs = 42
+        epochs = 100
         splitsize = len(trainloader) // 10
         print(f"SPLITSIZE: {splitsize}")
         try:
